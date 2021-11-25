fb79ac2994b19151f6448083d7794e7fa75cde9b
Ujszászi Károly <ujszaszi.karesz@gmail.com>
2021-11-25 22:32:05 +0100

    Refactor

--- app/inputfield/src/main/java/hu/ujszaszik/compose/inputfield/focus/FocusState.kt
+++
@@ -1,9 +0,0 @@
-package hu.ujszaszik.compose.inputfield.focus
-
-enum class FocusEvent {
-    REQUEST, CLEAR;
-
-    fun isRequested(): Boolean = this == REQUEST
-
-    fun isCleared(): Boolean = this == CLEAR
-}
